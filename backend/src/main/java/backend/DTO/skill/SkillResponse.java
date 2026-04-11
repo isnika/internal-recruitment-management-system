6c92428b11e2ad774c0bfdb3ffd691c869593c74
NguyenChanhKhue <chanhkhue7122005@gmail.com>
2026-04-11 23:47:47 +0700

    feat : add CRUD Skill APIs

---
+++ backend/src/main/java/backend/DTO/skill/SkillResponse.java
@@ -0,0 +1,15 @@
+package backend.DTO.skill;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SkillResponse {
+  private Long id;
+  private String name;
+}
