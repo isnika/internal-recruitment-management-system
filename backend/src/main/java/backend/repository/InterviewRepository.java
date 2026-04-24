33686e2b8ac5a25372c04193d2a569d4a106a9d6
KhoaDao <ahihi0388@gmail.com>
2026-04-24 22:47:18 +0700

    update InterviewReponsitory

--- backend/src/main/java/backend/repository/InterviewRepository.java
+++ backend/src/main/java/backend/repository/InterviewRepository.java
@@ -3,9 +3,12 @@ package backend.repository;
 import backend.entity.Interview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
 
   Optional<Interview> findByApplicationId(Long applicationId);
+
+  List<Interview> findByApplication_User_Id(Long userId);
 }
